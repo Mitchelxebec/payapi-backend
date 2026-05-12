36eda603149d44f823b132957fdd11a00b089073
Mitchel Okoh <mitchelokoh@gmail.com>
2026-05-12 12:55:35 +0100

    added the /service route

---
+++ src/controllers/getServiceInfo.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import { SERVICE_PRICES } from "../config/services";
+import { SUPPORTED_SERVICES } from "../models/Transaction";
+
+const getServiceInfo = (req: Request, res: Response, next: NextFunction) => {
+  const STROOPS_PER_XLM = 10_000_000;
+
+  res.status(200).json({
+    success: true,
+    data: SUPPORTED_SERVICES.map((name) => ({
+      name,
+      priceInXLM: (SERVICE_PRICES[name] / STROOPS_PER_XLM).toFixed(7),
+    })),
+  });
+};
+
+export default getServiceInfo;
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -10,10 +10,12 @@ import {
   revokeKeySchema,
   verifyPaymentSchema,
 } from "../validations/apiKeyValidation";
+import getServiceInfo from "../controllers/getServiceInfo";
 
 const router = Router();
 
 // ---  OPEN ROUTES (Management & Payment) ---
+router.get("/services", getServiceInfo);
 router.post("/verify-payment", validate(verifyPaymentSchema), verifyPayment);
 router.get("/keys", getKeys);
 router.post("/keys/revoke", validate(revokeKeySchema), revokeKey);
